21c6beb32b6b8c9f942d248118bff24ff18bf29a
Swarna-Saha324 <mumusaha85841@gmail.com>
2026-06-10 15:54:30 +0600

    Google Authentication is Done

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com', // গুগল লগইন প্রোফাইল ছবির জন্য সেফ জোন
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com', // 👈 ImgBB ইমেজ হোস্টনেম (নতুন যুক্ত করা হলো)
+      },
     ],
   },
   reactCompiler: true,
